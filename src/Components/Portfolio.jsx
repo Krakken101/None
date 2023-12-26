a171640f2f60231afee3aaea83e1820a3c5f7055
Brian Bange <144532338+Krakken101@users.noreply.github.com>
2023-12-26 11:33:28 +0300

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,30 +31,32 @@ const imageAltText = "wood";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "AWS CloudFormation Templates for Infrastructure as Code (IaC)",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Developed CloudFormation templates to automate the provisioning of AWS resources, ensuring consistent and scalable infrastructure.",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Serverless Computing with AWS Lambda",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Implemented serverless applications using AWS Lambda, including event triggers and integrations with other AWS services.",
   },
   {
-    title: "My Resume Site",
+    title: "AWS S3 Data Storage and Management",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Designed and implemented data storage solutions using Amazon S3, ensuring secure and scalable storage for various applications.",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "AWS Identity and Access Management (IAM) Best Practices",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Contributed to IAM policies and practices, ensuring secure access control and permissions for AWS resources.",
+  },
+  {
+    title: "Monitoring and Logging with AWS CloudWatch",
+    description:
+      "Implemented monitoring and logging solutions using AWS CloudWatch to ensure real-time insights into system performance and errors.",
   },
 ];
+
 
 const Portfolio = () => {
   return (
