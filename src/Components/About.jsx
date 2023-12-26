014f33ab9253f0504d84089b82f6e9b1cbe4640b
Brian Bange <144532338+Krakken101@users.noreply.github.com>
2023-12-26 12:12:55 +0300

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/textile-2918844_1280.jpg";
+import image from "src/images/textile-2918844_1280.jpg";
 
 const imageAltText = "textile";
 
